fb0b73dfad9064b10a653a02dc9819fd7f5c51a5
HaoLiHaiO <pinelwebdevelopment@gmail.com>
2021-08-06 13:11:19 +0000

    import sanityimagesource

--- CHANGES.md
+++ CHANGES.md
@@ -1,7 +1,13 @@
 # CHANGES
 
+- detailed descriptions for bug fixes which are not straightforward
+
 ## Released 06/08/21
 
 ### Fixes
 
 - add types for Sanity.io: npm install @sanity/types
+- add TS module declaration for sanity-block-content (taken from 
+https://github.com/nhi)
+- add SanityImageSource import (import { SanityImageSource } from 
+'@sanity/image-url/lib/types/types';) 
--- src/components/OnePost.tsx
+++ src/components/OnePost.tsx
@@ -2,7 +2,14 @@ import React, { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import sanityClient from '../client'
 import BlockContent from '@sanity/block-content-to-react'
+import imageUrlBuilder from "@sanity/image-url"
+import { SanityImageSource } from '@sanity/image-url/lib/types/types';
 
+const builder = imageUrlBuilder(sanityClient)
+
+const urlFor = (source: SanityImageSource)=> {
+    return builder.image(source)
+}
 
 const OnePost = () => {
     return <h2>OnePost Page</h2>
