8973c65acb07cf40ee1a6f2df11c35c83601f668
HaoLiHaiO <pinelwebdevelopment@gmail.com>
2021-08-06 13:02:13 +0000

    add types for sanity block content

--- src/components/OnePost.tsx
+++ src/components/OnePost.tsx
@@ -1,4 +1,8 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import sanityClient from '../client'
+import BlockContent from '@sanity/block-content-to-react'
+
 
 const OnePost = () => {
     return <h2>OnePost Page</h2>
