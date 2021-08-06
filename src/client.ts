2e7527cee320c89d9679df7e0c240f372963c881
HaoLiHaiO <pinelwebdevelopment@gmail.com>
2021-08-06 11:47:26 +0000

    add client

--- src/client.ts
+++ src/client.ts
@@ -1,2 +1,7 @@
-import sanityClient from "@sanity/client";
+import sanityClient from "@sanity/client"
 
+export default sanityClient({
+    projectId: "egicsc64",
+    dataset: "production",
+    useCdn: true,
+})
